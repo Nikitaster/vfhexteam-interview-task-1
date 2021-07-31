dd9ec8d00cdb14445a5e1c4e5c447a9297b02441
Nikita Gudkov <nikitaster2001@gmail.com>
2021-08-01 00:18:25 +0300

    realized method '_file_to_dict' for 'Decoder' class

--- convertor/decode_convertor.py
+++ convertor/decode_convertor.py
@@ -7,8 +7,14 @@ import glob
 class Decoder(AbstractFileConvertor):
     @staticmethod
     def _file_to_dict(filename) -> dict[str, str]:
-        # TODO: need to implement this functionality [1]
-        raise NotImplementedError('Not implemented [1]')
+        with open(filename) as file:
+            tuple_list = [
+                tuple(line.split(':', 1)) for line in file.readlines()
+            ]
+        return {
+            variable.strip().split(',')[0]: original.strip().lower() 
+            for original, variable in tuple_list
+        }
 
     def convert_text(self, input_text: str) -> str:
         for original, replace in self._latter_mapper.items():
