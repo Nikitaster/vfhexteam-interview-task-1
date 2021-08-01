e07bf506bd8b27d173b918cec61d749ef4d8b654
Nikita Gudkov <nikitaster2001@gmail.com>
2021-08-02 00:53:42 +0300

    implemented the convertor's methods in __main__.py file

--- convertor/__main__.py
+++ convertor/__main__.py
@@ -1,27 +1,38 @@
 import argparse
 import re
 import string
-KEY_FILE = 'key'
+import os
+from pathlib import Path
+
+import convertor
+from convertor.decode_convertor import Decoder
+from convertor.encode_convertor import Encoder
+
+
+KEY_FILE = Path(os.path.join(os.path.dirname(convertor.__file__)), 'key')
 
 
 def prepare_text_source(text: str):
-    return re.sub(string.punctuation, '', text.lower())
+    pattern = re.compile('[' + re.escape(string.punctuation) + ']')
+    return pattern.sub('', text.lower())
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('source_text',
-                        type=str,
-                        help='Returns encoded or decode text')
-    return parser.parse_args()
+def parse_args(func):
+    def wrapper():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('source_text',
+                            type=str,
+                            help='Returns encoded or decode text')
+        return func(parser.parse_args())
+    return wrapper
 
 
-def decode():
-    # use a prepare_text_source function for preparing!
-    # TODO: need to implement this functionality [4]
-    print('Opps... The decode function is not implemented')
+@parse_args
+def decode(args):
+    print(Decoder(KEY_FILE).convert_text(args.source_text))
 
 
-def encode():
-    # TODO: need to implement this functionality [5]
-    print('Opps... The encode function is not implemented')
+@parse_args
+def encode(args):
+    ambient_text_without_punctuation = prepare_text_source(args.source_text)
+    print(Encoder(KEY_FILE).convert_text(ambient_text_without_punctuation))
